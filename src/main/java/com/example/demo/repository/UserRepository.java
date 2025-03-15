00a855ddf84868da721b8104087d3fad94370758
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-03-15 17:34:22 +0900

    upload
    upload

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+import com.example.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository                            //< Entity 명 ,id 컬럼타입>
+public interface UserRepository  extends JpaRepository<User, Integer> {
+    User findByName(String username);
+    User findByAge(int age);
+    Optional<User> findByAgeLessThan(int age);
+}
